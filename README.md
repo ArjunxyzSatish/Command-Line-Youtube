fe6ddfb2b78ae5e5cb88ff2a2dc8289f4a34ef66
ArjunxyzSatish <arjunsatish@protonmail.com>
2020-12-27 21:07:17 +0530

    added personalised feed

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # TermTube
 
-This is a script that lets you watch Youtube videos on the command line! It uses the `python-mpv` library to do this. There is also an option in the script that lets you use the already installed `mpv` in your system which has been commented out. It also uses the `youtube-search` library. Take a look at the script to know what you need to have installed before you can run it. 
-
 Clone the directory and run `./termtube.py -h` from the TermTube directory to get instructions on how to use the script.
+
+This is a script that lets you watch Youtube videos on the command line! 
+
+This script now also gives you a personalised feed of youtube videos. It reads the channel names from a list that you pass as a variable and prints out the 3 latest videos of each channel. Use the `-f 'channel-list'` option to use this option. Make sure that the 'channel-list' file exists in the directory you're running the script from.
+
+It uses the `python-mpv` library to do this. There is also an option in the script that lets you use the already installed `mpv` in your system which has been commented out. It also uses the `youtube-search` library. Take a look at the script to know what you need to have installed before you can run it. 
+
--- termtube.py
+++ termtube.py
@@ -1,46 +1,96 @@
 #!/bin/env python
 
-from youtube_search import YoutubeSearch
+import uyts
+import requests
+from bs4 import BeautifulSoup
 import mpv
-import json
 import sys
 
 player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True, osc=True)
 
+videos = []
+
 def help():
     print("""
-    Usage: termtube [OPTIONS] [QUERY]
+    Usage: termtube [OPTIONS] [QUERY]/[Channel List]
 
     Options:
      -h, --help             prints this help message
       n                     where n is the number of video results you want displayed
+     -f
 
     Query:
      This is what the script looks for on YouTube. Please enter it in quotes, eg. 'Messi vs Ronaldo'
+
+    Channel List:
+     This is the file containing the names of the channels whose videos you want in your feed. Make sure you have only one channel per line. The program prints the latest 3 videos for each channel.
 """)
+
+def getVideos(channel):
+    search = uyts.Search(channel)
+    for res in search.results:
+        if res.resultType == 'channel':
+            channelID = res.id
+            channelName = res.title
+            break
+
+    url = "https://www.youtube.com/feeds/videos.xml?channel_id="+channelID
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, "lxml")
+    entries = soup.find_all("entry")
+    for i in range(3):
+        videos.append({"title":entries[i].title.text, "url":entries[i].link.attrs["href"], "creator": channelName})
+
+
+def playVideo(choice):
+    chosenVideo = videos[int(choice) - 1]
+    print("Playing: " + chosenVideo['title'])
+    chosenVideoLink = chosenVideo['url']
+    player.play(chosenVideoLink)
+    player.wait_for_playback()
+
+
+def displayVideos(videoList):
+    for x, video in enumerate(videos):
+        print(f'{x+1}. ' + videos[x]['title'] + ' : ' + videos[x]['creator'])
+
+    ch = input('Enter Choice: ')
+
 
 if len(sys.argv) == 1:
     help()
-    
-elif sys.argv[1] == '-h' or sys.argv[1] == '--help': 
+
+elif sys.argv[1] == '-h' or sys.argv[1] == '--help':
     help()
+
+elif sys.argv[1] == '-f' or sys.argv[1] == '--file':
+    subfile = sys.argv[2]
+    with open(subfile, 'r') as file:
+        listOfChannels = file.readlines()
+        for channel in listOfChannels:
+            getVideos(channel)
+    for x, video in enumerate(videos):
+        print(f'{x+1}. ' + videos[x]['title'] + ' : ' + videos[x]['creator'])
+
+    ch = input('Enter Choice: ')
+    playVideo(ch)
+
 
 else:
 
-    searchQuery = sys.argv[2]
-    number = int(sys.argv[1])
+    if sys.argv[1] != int:
+        number = 5
+        searchQuery = sys.argv[1]
+    else:
+        searchQuery = sys.argv[2]
+        number = int(sys.argv[1])
 
-    results = YoutubeSearch(searchQuery, max_results=number).to_json()
-    data = json.loads(results)
-    videos = data['videos']
-    for x, video in enumerate(videos):
-        print(f'{x+1}. ' + videos[x]['title'] + ' : ' + videos[x]['channel'])
+    search = uyts.Search(searchQuery)
+    for res in search.results:
+        if res.resultType == 'video':
+            videos.append({'title':res.title, 'url':'https://www.youtube.com/watch?v='+res.id, 'creator': res.author})
+            if len(videos) == number:
+                break
 
-    ch = input("Enter Choice: ")
-
-    chosenVideo = videos[int(ch) - 1]
-    print("Playing: " + chosenVideo['title'])
-    chosenVideoLink = 'https://www.youtube.com/watch?v=' + chosenVideo['id']
-
-    player.play(chosenVideoLink)
-    player.wait_for_playback()
+    displayVideos(videos)
+    playVideo(ch)
