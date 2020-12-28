ee5120a1c2a5742bd2ce4f4f92863ba68a712f4c
ArjunxyzSatish <arjunsatish@protonmail.com>
2020-12-28 12:07:45 +0530

    Added feature to get RSS links from channels in the channel file

--- README.md
+++ README.md
@@ -6,5 +6,7 @@ This is a script that lets you watch Youtube videos on the command line!
 
 This script now also gives you a personalised feed of youtube videos. It reads the channel names from a list that you pass as a variable and prints out the 3 latest videos of each channel. Use the `-f 'channel-list'` option to use this option. Make sure that the 'channel-list' file exists in the directory you're running the script from.
 
+You can also get RSS links for your favourite channels using the `-rss` option.
+
 It uses the `python-mpv` library to do this. There is also an option in the script that lets you use the already installed `mpv` in your system which has been commented out. It also uses the `youtube-search` library. Take a look at the script to know what you need to have installed before you can run it. 
 
--- termtube.py
+++ termtube.py
@@ -17,14 +17,29 @@ def help():
     Options:
      -h, --help             prints this help message
       n                     where n is the number of video results you want displayed
-     -f
+     -f, --file             specify the file with the list of channels on it and it fetches the 3 most recent videos of these channels.
+     -rss, --get-rss        specify the file with the list of channels on it and it fetches the RSS links of these channels and stores them in another file. It will ask you to name this new file.
 
     Query:
      This is what the script looks for on YouTube. Please enter it in quotes, eg. 'Messi vs Ronaldo'
 
     Channel List:
-     This is the file containing the names of the channels whose videos you want in your feed. Make sure you have only one channel per line. The program prints the latest 3 videos for each channel.
+     This is the file containing the names of the channels whose videos you want in your feed. Make sure you have only one channel per line. The program prints the latest 3 videos for each channel. If this is used with the -rss or --get-rss option, it gets the RSS links of these channels and stores them in a new file in the same directory. It will ask you to name the new file containing the RSS links of these channels. 
 """)
+
+def getRSS(channelFile):
+    RSSlinks = []
+    with open(channelFile, 'r') as file:
+        listOfChannels = file.readlines()
+        for channel in listOfChannels:
+            search = uyts.Search(channel)
+            for res in search.results:
+                if res.resultType == 'channel':
+                    channelID = res.id
+                    channelName = res.title
+                    RSSlinks.append({"creator":channelName, "link":"https://www.youtube.com/feeds/videos.xml?channel_id="+channelID})
+                    break
+    return RSSlinks
 
 def getVideos(channel):
     search = uyts.Search(channel)
@@ -73,6 +88,15 @@ elif sys.argv[1] == '-f' or sys.argv[1] == '--file':
     displayVideos(videos)
     ch = input('Enter Choice: ')
     playVideo(ch)
+
+elif sys.argv[1] == '-rss' or sys.argv[1] == '--get-rss':
+    subfile = sys.argv[2]
+    print("Getting RSS feed links for channels in '" + subfile + "' file..")
+    RSSlinks = getRSS(subfile)
+    RSSfile = input("Enter the name of the file you want to store the links in: ")
+    with open(RSSfile, 'a') as file:
+        for channel in RSSlinks:
+            file.writelines(channel['creator'] + " : " + channel['link'] + "\n")
 
 
 else:
